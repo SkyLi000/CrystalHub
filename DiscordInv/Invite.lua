a9fff10ea9fb366ad18597dc96f9fd9b7053c293
SkyLi000 <97918490+SkyLi000@users.noreply.github.com>
2022-10-13 18:30:07 +1300

    Create Invite.lua

---
+++ DiscordInv/Invite.lua
@@ -0,0 +1,5 @@
+local inviteModule = loadstring(game:HttpGet("https://raw.githubusercontent.com/SkyLi000/GalaxyScript/main/DiscordInv/InviteModule.lua"))()
+inviteModule.Prompt({
+	name = "Galaxy Script Center",
+	invite = "https://discord.gg/QbQ3aFRbbJ",
+})
