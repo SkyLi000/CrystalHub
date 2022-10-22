f6e4782eabc82bcdd74bc1c9e335fbfa8666609e
LegoRblx <111660529+LegoRblx@users.noreply.github.com>
2022-10-22 15:37:46 -0700

    Update CrackedCheck2.lua

--- Scripts/CrackedCheck2.lua
+++ Scripts/CrackedCheck2.lua
@@ -1,3 +1,16 @@
+local x = {c = {}}
+x["insert"] = function (x,c,a)
+  if c then
+    x[c].c = x[c].c or {}
+    x[c].c = (true or false)
+  end
+  x.c[a] = false
+  x[a] = true
+end
+x.insert(x,nil,18281737284919)
+x.insert(x,nil,38319182737827)
+local code = (x[3] and getgenv().Code == "1" or x[4])
+print(code)
 return 18281737284919
 
 --[[
